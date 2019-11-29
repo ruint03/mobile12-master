29e76c88d7b6b903c368e20113745cfbb6a3e423
ruint03 <qlsbinu@yonsei.ac.kr>
2019-11-29 13:34:52 +0900

    getIdentifier 추가

--- app/src/main/java/com/example/choi/iqproject/Onelayout.java
+++ app/src/main/java/com/example/choi/iqproject/Onelayout.java
@@ -1,5 +1,7 @@
 package com.example.choi.iqproject;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,13 +13,27 @@ import android.widget.RadioGroup;
 
 
 public class Onelayout extends Fragment {
+    Context mContext;
     View v;
     RadioGroup[] rg;
+    String id = "R.id.rg_";
+    String packName = mContext.getPackageName();
+    public Onelayout() {}
+    @SuppressLint("ValidFragment")
+    public Onelayout(Context mContext) {
+        this.mContext = mContext;
+    }
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v= inflater. inflate(R.layout.one_layout,container,false);
-        rg[0] = (RadioGroup)v.findViewById(R.id.rg_1);
+        for(int i = 1; i<=34;i++){
+            int k = getResources().getIdentifier("rg_"+i,"id",packName);
+            rg[i] = v.findViewById(k);
+        }
 return v;
     }
 }
